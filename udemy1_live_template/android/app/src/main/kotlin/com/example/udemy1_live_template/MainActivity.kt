884f1ae710f56c98b90f51d5acfd8614954e654b
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-01-28 20:50:29 +0700

    Udemy Flutter App Study

---
+++ udemy1_live_template/android/app/src/main/kotlin/com/example/udemy1_live_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy1_live_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udemy1_live_template/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp( // material app is an application that uses a material
+    home: MyApp(),
+  ));
+}
+
+// State is just simply remembering what's going on 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  // BuildContext = this is the context of which this method is being run, whether it's startup, shut down, refresh, etc.
+  Widget build(BuildContext context) {
+    return Scaffold( // Scaffold is a structure on which you are going to build your material application
+      appBar: AppBar(
+        title: Text('Udemy Learn'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: [
+              Text('Hello World')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
