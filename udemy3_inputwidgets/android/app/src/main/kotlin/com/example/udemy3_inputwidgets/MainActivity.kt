b590df9fc8f65c93ed2ef56e1f7a2e10aa2728dc
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-01-29 17:59:14 +0700

    replacing app and Text Field Code

--- udemy3_inputwidgets/android/app/src/main/kotlin/com/example/udemy3_inputwidgets/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.udemy3_inputwidgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ udemy4_textfield/android/app/src/main/kotlin/com/example/udemy4_textfield/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy4_textfield
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
