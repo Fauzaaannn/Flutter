7ab1051a2a3171376cc3912ee07c83d719a13f65
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-01-31 14:22:18 +0700

    how to make Switches

--- udemy7_switches/lib/main.dart
+++ udemy7_switches/lib/main.dart
@@ -16,19 +16,18 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  String _value = 'Hello World';
+  bool _value1 = false;
+  bool _value2 = false;
 
-  void _onPressed() {
-    setState((){
-      _value = 'My Name is Fauzan';
-    });
-  }
+  void _onChanged1(bool value) => setState(() => _value1 = value );
+  void _onChanged2(bool value) => setState(() => _value2 = value );
 
   @override
   // BuildContext = this is the context of which this method is being run, whether it's startup, shut down, refresh, etc.
   Widget build(BuildContext context) {
     return Scaffold( // Scaffold is a structure on which you are going to build your material application
       appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
         title: Text('Udemy Learn'),
       ),
       body: Container(
@@ -36,8 +35,12 @@ class _MyAppState extends State<MyApp> {
         child: Center(
           child: Column(
             children: <Widget> [
-              Text(_value),
-              ElevatedButton(onPressed: _onPressed, child: Text('Click me'))
+              Switch(value: _value1, onChanged: _onChanged1),
+              SwitchListTile(
+                value: _value2, 
+                onChanged: _onChanged2,
+                title: Text('Hello World', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),),
+                )
             ],
           ),
         ),
