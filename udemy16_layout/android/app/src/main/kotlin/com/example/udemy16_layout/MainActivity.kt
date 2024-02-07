357ed49da32740824d54b7797f1b1cc698adfd24
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-02-07 11:25:38 +0700

    new app Layout

---
+++ udemy16_layout/android/app/src/main/kotlin/com/example/udemy16_layout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy16_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udemy16_layout/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp( // material app is an application that uses a material
+    home: MyApp(),
+  ));
+}
+
+// State is just simply remembering what's going on 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  String _value = 'Hello World';
+
+  void _onPressed() {
+    setState((){
+      _value = 'My Name is Fauzan';
+    });
+  }
+
+  @override
+  // BuildContext = this is the context of which this method is being run, whether it's startup, shut down, refresh, etc.
+  Widget build(BuildContext context) {
+    return Scaffold( // Scaffold is a structure on which you are going to build your material application
+      appBar: AppBar(
+        title: Text('Udemy Learn'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget> [
+              Text(_value),
+              ElevatedButton(onPressed: _onPressed, child: Text('Click me'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
