ad9cba9d13c8423a203c00c4fce3ea0146b5205c
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-02-07 19:16:30 +0700

    Rows and Columns with textfield

--- udemy16_layout/lib/main.dart
+++ udemy16_layout/lib/main.dart
@@ -16,28 +16,42 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  String _value = 'Hello World';
-
-  void _onPressed() {
-    setState((){
-      _value = 'My Name is Fauzan';
-    });
-  }
+  TextEditingController _user = new TextEditingController();
+  TextEditingController _pass = new TextEditingController();
 
   @override
   // BuildContext = this is the context of which this method is being run, whether it's startup, shut down, refresh, etc.
   Widget build(BuildContext context) {
     return Scaffold( // Scaffold is a structure on which you are going to build your material application
       appBar: AppBar(
-        title: Text('Udemy Learn'),
+        backgroundColor: Colors.blue,
+        title: Text('Udemy Learn', style: TextStyle(color: Colors.white),),
       ),
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
             children: <Widget> [
-              Text(_value),
-              ElevatedButton(onPressed: _onPressed, child: Text('Click me'))
+              Text('Please Login'),
+              Row(
+                children: <Widget>[
+                  Text('Username: '),
+                  Expanded(child: TextField(controller: _user,))
+                  // the label has a fixed sized, the text field is considered a greedy widget, meaning it's going to take as much space as you give it and it's just going to keep going
+                  // expanded = it says it expands to take up the rest of the screen or the rest of the widget that we're a child of and then fills the text field to fill that
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Password: '),
+                  Expanded(child: TextField(controller: _pass, obscureText: true,))
+                ],
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(12.0),
+                child: ElevatedButton(onPressed: () => print('Login ${_user.text}'), child: Text('Click me')),
+              )
             ],
           ),
         ),
